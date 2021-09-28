39a4fe77059d8fd8e63cbf3b5c6df68df98440ba
Nicolas Jimenez <nicolasjimenez.d11@gmail.com>
2021-09-27 19:40:16 -0500

    Agregar instalacion de tailwind

--- src/Pages/gestionVentas.js
+++ src/Pages/gestionVentas.js
@@ -1,5 +1,6 @@
 import './Styles/gestionVentas.css';
 import{Link} from "react-router-dom";
+import administrador from "../media/usuarios.png"
 
 function GestVentas() {
     return(
@@ -7,6 +8,7 @@ function GestVentas() {
             <div className="body">
                 <h1>T-SOLUTIONS</h1>
                 <p>Gestion de ventas</p>
+                <img src={administrador} alt="Logo de administrador"></img> 
                 <button className="btnAtras"><Link to='/'>Atras</Link></button>
             </div>
         </div>
---
+++ src/components/SideBarAdmin.jsx
@@ -0,0 +1,14 @@
+
+const SideBarAdmin=()=>{
+    return(
+        <div>
+           <section>
+            <img src="./media/admin.png" alt="Logo de administrador"></img> 
+            <samp></samp>   
+            </section>
+        </div>
+    );
+
+}
+
+export default SideBarAdmin;
