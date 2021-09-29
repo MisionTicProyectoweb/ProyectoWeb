3dc1ffe36663ee43e97c871222edd346e8645415
Yineth Duarte <88860062+345467Y@users.noreply.github.com>
2021-09-29 18:18:57 -0500

    algunos cambios en ventas

--- src/Pages/Bienvenida.jsx
+++ src/Pages/Bienvenida.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Tecnología from "media/Tecnología.png"
 
 const Bienvenida = () => {
     return (
         <div>
-            <h1 className="text-gray-800 flex justify-center font-extrabold">Bienvenido a T-SOLUTIONS</h1>
+            <h1 className="text-purple-900 flex justify-center font-extrabold">Bienvenido a T-SOLUTIONS</h1>
+            <p1>Tienda dedicada a la venta y distribución de productos tecnológicos</p1>
+            <div className="w-40 ml-20 mt-20">
+            <img src={Tecnología} alt="Logo de tecnología" />
+            </div>
         </div>
     )
 }
--- src/components/SidebarVendedor.jsx
+++ src/components/SidebarVendedor.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import Productos from "media/productos.png"
+import Usuarios from "media/usuarios.png"
 import IconoAdminis from "media/iconoAdmin.png"
+import Ventas from "media/ventas.png"
 
 const SidebarVendedor = () => {
     return (
-        <div className="w-72 border bg-purple-500 flex flex-col justify-between h-full">
-            <div className="w-12 ml-4 mt-4">
-                <span className="ml-22 mt-12">Admin</span>
+        <div className="w-80 border bg-purple-500 flex flex-col justify-between h-full">
+            <div className="w-20 ml-6 mt-6">
                 <img src={IconoAdminis} alt="Logo de usuario" />
-             
+                <span className="ml-30 mt-20">Administrador</span>
             </div>
+            <div className="w-20 ml-6 mt-6">
+                <img src={Ventas} alt="Logo de ventas"/>
+                <span className="ml-22 mt-12">Ventas</span>
+                </div>
+                <div className="w-20 ml-6 mt-6">
+                <img src={Usuarios} alt="Logo de usuarios"/>
+                <span className="ml-22 mt-12">Usuarios</span>
+                </div>
+                <div className="w-20 ml-6 mt-6">
+                <img src={Productos} alt="Logo de productos"/>
+                <span className="ml-22 mt-12">Productos</span>
+                </div>
         </div>
     )
 }
