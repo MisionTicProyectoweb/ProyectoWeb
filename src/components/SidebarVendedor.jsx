4ece71538fa5f8fecc8b7b18b4cc4be71996c5a9
manuel <manuelguma25@gmail.com>
2021-09-28 22:24:28 -0500

    arreglar icono de admin

--- src/components/SidebarVendedor.jsx
+++ src/components/SidebarVendedor.jsx
@@ -4,8 +4,10 @@ import IconoAdminis from "media/iconoAdmin.png"
 const SidebarVendedor = () => {
     return (
         <div className="w-72 border bg-purple-500 flex flex-col justify-between h-full">
-            <div>
+            <div className="w-12 ml-4 mt-4">
+                <span className="ml-22 mt-12">Admin</span>
                 <img src={IconoAdminis} alt="Logo de usuario" />
+             
             </div>
         </div>
     )
