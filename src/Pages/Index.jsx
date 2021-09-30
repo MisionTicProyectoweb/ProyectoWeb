0eb57af4af51f606a4a752464713b9e8e788adaf
manuel <manuelguma25@gmail.com>
2021-09-29 22:25:02 -0500

    cambio de color de letra

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,5 +1,6 @@
 import './Styles/Login.css';
 import imagenLogin from 'media/login.svg';
+import imagenlog from 'media/imglogin.png'
 import GoogleLogin from 'react-google-login'
 import googlelogo from 'media/googlelogin.svg'
 import{Link} from "react-router-dom";
@@ -7,15 +8,15 @@ import{Link} from "react-router-dom";
 function Login() {
     return(
         <div className="body">
-           <div className="cen">
-               <div className="fondo">
-                   <span className="p-2 font-sick font-semibold text-6xl flex justify-center">T-SOLUTIONS</span>
+           <div className="cen shadow-md bg-purple-500 ">
+               <div className="fondo  ">
+                   <span className="p-2  font-extrabold text-gray-700 text-6xl flex justify-center ">T-SOLUTIONS</span>
                    <img className="imagenLogo"src={imagenLogin} alt="Logo"/>
                </div>
                <form>
                    <div>
-                       <span className="p-0 font-semibold text-4xl flex justify-center">ACCEDER</span>
-                       <span className="p-0 text-sm flex justify-center">Ir a la tienda tecnológica</span>
+                       <span className="p-0 font-semibold text-4xl flex justify-center  font-extrabold text-gray-700">ACCEDER</span>
+                       <span className="p-0 text-sm flex justify-center font-extrabold text-gray-700 text-2xl ">Ir a la tienda tecnológica</span>
                    </div>
                     <GoogleLogin 
                         clientId="" 
@@ -23,9 +24,9 @@ function Login() {
                         render = {
                             renderProps => (
                                 <Link to="/bienvenida">
-                                    <button className="items-center justify-center flex bg-white text-black text-sm rounded-lg p-1 m-3 rounded-md">
+                                    <button className="  items-center justify-center flex bg-white text-black text-sm rounded-lg p-1 m-3 rounded-md  hover:bg-purple-100 shadow-md-col  ">
                                         <img className="pl-1 w-1/6" src={googlelogo} alt="LogoGoogle"/>
-                                        <span className="pl-2 pr-2">Iniciar sesión con Google</span>
+                                        <span className="pl-1 pr-1  font-extrabold text-gray-700   ">Continúa con Google</span>
                                     </button>
                                 </Link>
                             )
