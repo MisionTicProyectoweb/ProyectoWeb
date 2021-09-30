21c24137794f3a8235c087b7f55457dfbabc4d10
Nicolas Jimenez <nicolasjimenez.d11@gmail.com>
2021-09-30 11:35:59 -0500

    Modificar el titulo y boton de la gestion de usuarios

--- src/Pages/GestionUsuarios.jsx
+++ src/Pages/GestionUsuarios.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const GestionUsuarios = () => {
     return (
         <div className="h-full w-full ">
-           <navbar className="flex flex-row m-5">
-               <h1 className="text-gray-900 font-bold text-5xl ml-5 mr-96">Listado de usuarios</h1>
+           <navbar className="flex flex-row justify-between bg-indigo-300 p-7">
+               <h1 className="text-gray-900 font-bold text-5xl ">Listado de usuarios</h1>
                <button className="bg-indigo-500 text-white rounded-lg p-1 hover:bg-indigo-600 ">Modificar</button>
            </navbar>
            
