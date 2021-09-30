f1a4a8506ed807e0a0ae4583d48c4b9ed18611cb
Nicolas Jimenez <nicolasjimenez.d11@gmail.com>
2021-09-29 22:50:56 -0500

    Agregar Titulo para la lista de usuarios

--- src/Pages/GestionUsuarios.jsx
+++ src/Pages/GestionUsuarios.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const GestionUsuarios = () => {
     return (
-        <div>
-            Pagina para la gestion de usuarios
+        <div className="h-full w-full ">
+           <navbar className="flex flex-row m-5">
+               <h1 className="text-gray-900 font-bold text-5xl ml-5 mr-96">Listado de usuarios</h1>
+               <button className="bg-indigo-500 text-white rounded-lg p-1 hover:bg-indigo-600 ">Modificar</button>
+           </navbar>
+           
         </div>
     )
 }
--- src/components/SideBarAdmin.jsx
+++ src/components/SideBarAdmin.jsx
@@ -9,21 +9,21 @@ const SideBarAdmin=()=>{
     return(
         <div className="flex flex-col w-72 border bg-purple-500 pt-5 pb-5 items-center">
         <ul className="flex flex-col justify-between h-full ">
-            <li className="flex flex-col items-center ">
+            <li className="flex flex-col items-center">
                 <img className="w-20 cursor-pointer" src={IconoAdminis} alt="Icono administrador" />
                 <span className="p-1 text-gray-900 font-semibold ">Nombre del administrador</span>
             </li>
             <li className="flex flex-col items-center ">
-              <img className="w-20 cursor-pointer" src={IconoVent} alt="Logo de ventas" />
+              <img className="w-12 cursor-pointer" src={IconoVent} alt="Logo de ventas" />
                 <span className="p-1 text-gray-900 font-semibold">Gestion de Ventas</span>
             </li>
             <li className="flex flex-col items-center ">
-                <img className="w-20 cursor-pointer" src={IconoProd} alt="Logo de productos" />
+                <img className="w-12 cursor-pointer" src={IconoProd} alt="Logo de productos" />
                 <span className="p-1 text-gray-900 font-semibold">Gestion de Productos</span>
             </li>
                 <Link to="/bienvenida/usuarios">
             <li className="flex flex-col items-center ">
-                <img className="w-20 cursor-pointer" src={IconoUsuarios} alt="Logo de usuarios" />
+                <img className="w-12 cursor-pointer" src={IconoUsuarios} alt="Logo de usuarios" />
                 <span className="p-1 text-gray-900 font-semibold">Gestion de Usuarios</span>
             </li>
                 </Link>
