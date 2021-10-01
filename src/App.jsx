e31f0ce893e589cbe074fd437e6ca34b0e03a9a9
Juan Felipe Osorio <jfelipe.osorio@udea.edu.co>
2021-10-01 18:24:09 -0500

    Gestionar ventas con registro en tabla detalle

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import Index from 'Pages/Index';
-import GestionVentas from 'Pages/GestionVentas';
-import GestionProductos from 'Pages/GestionProductos';
-import GestionUsuarios from 'Pages/GestionUsuarios';
-import ListVentas from 'Pages/listaVentas';
-import Bienvenida from 'Pages/bienvenida';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import LayoutPrivado from 'layouts/LayoutPrivado';
-import LayoutPublico from 'layouts/LayoutPublico';
+import Index from "Pages/Index";
+import GestionVentas from "Pages/GestionVentas";
+import GestionProductos from "Pages/GestionProductos";
+import GestionUsuarios from "Pages/GestionUsuarios";
+import ListVentas from "Pages/listaVentas";
+import Bienvenida from "Pages/bienvenida";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LayoutPrivado from "layouts/LayoutPrivado";
+import LayoutPublico from "layouts/LayoutPublico";
 
 
-export default function App(){  
-    return(
-       <div>
-           <Router>
-               <Switch>
-                    <Route path="/listaventas">
-                        <ListVentas/>
-                    </Route>
-                    <Route path="/ventas">
-                        <GestionVentas/>
-                    </Route>
-                    <Route path="/productos">
-                        <GestionProductos/>
-                    </Route>
-                    <Route path="/usuarios">
-                        <GestionUsuarios/>
-                    </Route>
-                    <Route path={["/bienvenida"]}>
-                        <LayoutPrivado>
-                            <Switch>
-                                <Route path="/bienvenida">
-                                    <Bienvenida/>
-                                </Route>
-                            </Switch>
-                        </LayoutPrivado>
-                    </Route>
-                    <Route path="/">
-                        <LayoutPublico>
-                            <Index/>
-                        </LayoutPublico>
-                    </Route>
-               </Switch>
-           </Router>
-       </div>
-    );
+
+
+export default function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/listaventas">
+            <ListVentas />
+          </Route>
+          <Route path="/ventas">
+            <GestionVentas />
+          </Route>
+          <Route path="/productos">
+            <GestionProductos />
+          </Route>
+          <Route path="/usuarios">
+            <GestionUsuarios />
+          </Route>
+          <Route path={["/bienvenida"]}>
+            <LayoutPrivado>
+              <Switch>
+                <Route path="/bienvenida">
+                  <Bienvenida />
+                </Route>
+              </Switch>
+            </LayoutPrivado>
+          </Route>
+          <Route path="/">
+            <LayoutPublico>
+              <Index />
+            </LayoutPublico>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
