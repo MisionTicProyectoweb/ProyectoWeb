9e2a73251be94c760f389642a571c5f33ecf037d
Nicolas Jimenez <nicolasjimenez.d11@gmail.com>
2021-10-01 23:44:27 -0500

    Eliminar video de prueba de la carpeta media

--- src/Pages/bienvenida.jsx
+++ src/Pages/bienvenida.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Video from "media/videoBienvenida.mp4"
+
 const Bienvenida = () => {
     return (
-        <div>
-            Bienvenida
+        <div className="h-full w-full relative ">
+            <h1 className="text-gray-800 flex justify-center font-extrabold text-5xl relative z-2">Bienvenido a T-SOLUTIONS</h1>    
+            <video className="absolute  object-cover" src={Video} muted autoPlay loop>
+            </video>
+            <div className="h-full w-full relative  "></div>
         </div>
     )
 }
