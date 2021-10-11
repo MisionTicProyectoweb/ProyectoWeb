264c93d6fbe3804756d1c3b700a5088bc8fe8590
Faber Hoyos <faberhoyos01@gmail.com>
2021-10-11 15:46:48 -0500

    cambios sin conflictos

---
+++ src/utils/api.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export const obtenerVentas = async (setVentas, setEjecutarVentas = () => {}) => {
+
+  const options = { method: 'GET', url: 'http://localhost:5000/ventas/' };
+
+  await axios
+    .request(options)
+    .then(function (response) {
+      setVentas(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  setEjecutarVentas(false);
+};
+
+export const obtenerUsuarios = async (setVentas, setEjecutarUsuarios = () => {}) => {
+
+  const options = { method: 'GET', url: 'http://localhost:5000/usuarios/' };
+
+  await axios
+    .request(options)
+    .then(function (response) {
+      setVentas(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  setEjecutarUsuarios(false);
+};
