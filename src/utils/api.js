3887693d0a184a5040ea8293b546a3cf3f5432f6
mreyesq <marcereyesq@gmail.com>
2021-10-12 20:02:44 -0500

    Aceptar cambios

--- src/utils/api.js
+++ src/utils/api.js
@@ -39,7 +39,7 @@ export const crearProducto = async (data, successCallback, errorCallback) => {
 };
 
 export const obtenerProductos = async (successCallback, errorCallback) => {
-  const options = { method: 'GET', url: 'http://localhost:5000/productos/'};
+  const options = { method: 'GET', url: 'http://localhost:5000/productos/' };
   await axios.request(options).then(successCallback).catch(errorCallback);
 };
 
