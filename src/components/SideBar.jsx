8011fdd4950abd74fed0bf09a6b8726c2dd25240
Juan Felipe Osorio <jfelipe.osorio@udea.edu.co>
2021-10-13 11:18:27 -0500

    actualizacion de cambios

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import logo from 'media/logoTSolutions.png';
-import useActiveRoute from 'hooks/useActiveRoute';
-=======
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "media/logoTSolutions.png";
 import useActiveRoute from "hooks/useActiveRoute";
->>>>>>> 9dd1f6f466fccaf28ec9e8a81c66c0458db981ae
 
 let estado, setEstado;
 const Sidebar = (navegar) => {
@@ -22,71 +15,6 @@ const Sidebar = (navegar) => {
 };
 
 const Ruta = ({ icono, ruta, nombre }) => {
-<<<<<<< HEAD
-    const isActive = useActiveRoute(ruta);
-    return (
-      <Link to={ruta}>
-        <button
-          className={`my-4 text-base bg-${
-            isActive ? "indigo" : "bg-indigo-600"
-          }-700 w-full flex items-center py-3 text-sm font-semibold text-white dark-mode:hover:text-white dark-mode:text-white hover:bg-indigo-300 focus:shadow-outlin`}
-        >
-          <i className={`${icono} w-8`} />
-          {nombre}
-        </button>
-      </Link>
-    );
-};
-const show = () => {
-    return (
-        <div className="flex flex-col items-center justify-center bg-indigo-500">
-            <nav className='hidden lg:flex lg:w-56 border border-indigo-500 h-full flex-col bg-indigo-500 w-52'>
-                <Link to='/admin'>
-                    <img className='w-40 ml-9 mt-4 mb-10' src={logo} alt="imagen" />
-                </Link>
-                <div className='my-6 '>
-                    <Ruta icono='fas fa-users-cog' ruta='/admin/dashboard' nombre='DashBoard' />
-                    <Ruta icono='fas fa-cart-plus' ruta='/admin/productos' nombre='Productos' />
-                    <Ruta icono='fas fa-clipboard-list' ruta='/admin/ventas' nombre='Ventas' />
-                    <Ruta icono='fas fa-users' ruta='/admin/usuarios' nombre='Usuarios' />
-                </div>
-            </nav>
-            <div className="w-full bottom-0">
-                <Ruta icono='fas fa-sign-out-alt' ruta='/inicio' nombre='Salir' />
-            </div>
-        </div>);
-}
-
-const hide = () => {
-    return (
-        <div className="mt-48 flex-col bg-indigo-500 items-center">
-            <nav className='flex items-center lg:flex lg:w-14 border border-indigo-500 h-full flex-col bg-indigo-500'>
-                <div>
-                    <Ruta icono='fas fa-users-cog' ruta='/admin/dashboard' />
-                    <Ruta icono='fas fa-cart-plus' ruta='/admin/productos' />
-                    <Ruta icono='fas fa-clipboard-list' ruta='/admin/ventas' />
-                    <Ruta icono='fas fa-users' ruta='/admin/usuarios' />
-                </div>
-            </nav>
-            <div className="w-full flex items-center justify-center">
-                <Ruta icono='fas fa-sign-out-alt' ruta='/inicio' />
-            </div>
-        </div>);
-}
-
-const btnHide = () => {
-    return (
-        <div className="bg-indigo-500 text-white my-4"
-            onClick={() => {
-                setEstado(!estado);
-            }}>
-            <i className={`mx-8 fas fa-${estado ? 'chevron-left' : 'bars'} hover:text-black `} />
-        </div>
-    );
-};
-
-export default Sidebar;
-=======
   const isActive = useActiveRoute(ruta);
   return (
     <Link to={ruta}>
@@ -173,4 +101,3 @@ const btnHide = () => {
 };
 
 export default Sidebar;
->>>>>>> 9dd1f6f466fccaf28ec9e8a81c66c0458db981ae
