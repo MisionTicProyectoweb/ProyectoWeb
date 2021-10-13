37d9593c68ffe988ac6fdc18edd2d6522203e1e9
Faber Hoyos <faberhoyos01@gmail.com>
2021-10-12 21:42:11 -0500

    filtro usuarios

--- src/Pages/listaUsuarios.jsx
+++ src/Pages/listaUsuarios.jsx
@@ -13,25 +13,42 @@ import { unstable_renderSubtreeIntoContainer } from "react-dom";
 const Usuarios = () => {
 
   const [usuarios, setUsuarios] = useState([]);
+  const [busqueda, setBusqueda] = useState('');
+  const [usuariosFiltrados, setUsuariosFiltrados] = useState([]);
   const [ejecutarConsulta, setEjecutarConsulta] = useState(true);
   useEffect(() => {
       //Obtener Usuarios desde el backend
       if (ejecutarConsulta){
           obtenerUsuarios(setUsuarios,setEjecutarConsulta);
       }
+      setUsuariosFiltrados(usuarios);
   }, [ejecutarConsulta]);
+
+  useEffect(()=>{
+    setUsuariosFiltrados(
+      usuarios.filter((elemento) =>{
+        return JSON.stringify(elemento).toLowerCase().includes(busqueda.toLowerCase());
+      })
+    );
+  }, [busqueda]);
+
   return (
     <div className="flex h-full w-full flex-col items-center justify-start ">
       <NavBarFull titulo="Listado de Usuarios" subtitulo={`Usuarios: ${usuarios.length}`}/>
       <div className="mb-2 flex items-center justify-center w-full h-20">
         <label className="text-base font-semibold mr-5 text-black">Buscar:</label>
         <svg width="24" height="24" fill="none" class="text-gray-400 group-hover:text-gray-500 transition-colors duration-200"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-        <input type="text" className="p-2 ml-4 w-96 h-10 rounded-lg border shadow-md" placeholder="Buscar por ..."></input>
-        <select name="select" className="font-semibold text-center ml-4 border h-10 rounded-lg shadow-md">
-          <option value="value1" selected>Cedula</option>
-          <option value="value2">Nombre</option>
-          <option value="value3">Apellido</option>
-          <option value="value3">Correo</option>
+        <input 
+            value={busqueda}
+            onChange={(e) => setBusqueda(e.target.value)} 
+            type="text" 
+            className="p-2 ml-4 w-96 h-10 rounded-lg border shadow-md" 
+            placeholder="Buscar por ..."></input>
+        <select id="busPor" name="select" className="font-semibold text-center ml-4 border h-10 rounded-lg shadow-md">
+          <option value="ccUsuario" selected>Cedula</option>
+          <option value="nombre">Nombre</option>
+          <option value="apellido">Apellido</option>
+          <option value="correo">Correo</option>
         </select>
         <Link to="/admin/usuarios/gestionusuario">
           <button type="button" className="bg-indigo-500 text-white transform hover:scale-110 hover:bg-indigo-600   float-left ml-20 flex items-center p-2 rounded-lg border shadow-md">
@@ -42,7 +59,9 @@ const Usuarios = () => {
         </Link>
       </div>
       <div className="overflow-y-scroll">
-        <TablaUsuarios listaUsuarios={usuarios} setEjecutarConsulta={setEjecutarConsulta}/>
+        <TablaUsuarios 
+          listaUsuarios={usuariosFiltrados} 
+          setEjecutarConsulta={setEjecutarConsulta}/>
         <ToastContainer position="bottom-center" autoClose={5000} />
       </div>
     </div>
